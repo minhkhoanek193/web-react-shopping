9eb0703a7f4a2c1dbe8d4556ccec14a9e3907953
minhkhoanek193 <dangminhkhoa193@gmail.com>
2023-12-21 00:27:20 +0700

    web-react

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.urkxs2l.mongodb.net', // DOI 
+  DB_USER: 'dangminhkhoa193', // DOI 
+  DB_PASS: 'jJ5nO65XmnVi32dI',  // DOI 
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: '<jwt_secret>',
+  JWT_EXPIRES: '31565890000000', // in milliseconds
+  EMAIL_USER: 'dangminhkhoa2222@gmail.com',  // DOI 
+  EMAIL_PASS: 'mnmumasrcbpofnoq'    // DOI 
+}; 
+module.exports = MyConstants;
